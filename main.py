64d6f4bb1a05b12c4c9a251dc6b829a4657b1e19
santi-nue <68065589+santi-nue@users.noreply.github.com>
2025-12-29 12:48:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium_stealth import stealth
+from urllib.parse import urljoin, urlparse
+from webdriver_manager.chrome import ChromeDriverManager
 import time
 
 
